c33cdf9f76a879b4309734af10c400fe6ebb9e05
Giovanne Mendonca <giovannemendonca@outlook.com>
2024-10-27 17:27:40 -0300

    conectando ao firestone

---
+++ src/app/app.module.ts
@@ -0,0 +1,36 @@
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {ButtonComponent} from './components/button/button.component';
+import {LoginComponent} from './pages/login/login.component';
+import {FormsModule} from "@angular/forms";
+import {HomeComponent} from './pages/home/home.component';
+import {MenuComponent} from './components/menu/menu.component';
+import {MatIconModule} from "@angular/material/icon";
+import {MatProgressSpinner} from "@angular/material/progress-spinner";
+import {AngularFireModule} from "@angular/fire/compat";
+import {environment} from "../environments/environment";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ButtonComponent,
+    LoginComponent,
+    HomeComponent,
+    MenuComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    MatIconModule,
+    MatProgressSpinner,
+    AngularFireModule.initializeApp(environment.firebaseConfig)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+}
---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,15 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent implements OnInit {
+
+  username: string | null
+
+  ngOnInit() {
+    this.username = sessionStorage.getItem("user")
+  }
+}
